cddc5ae8c76d5489e052cd3816bdb64e33d137f1
Alexander Halim <akhalim@ucsc.edu>
2024-11-03 13:17:03 -0800

    adding button with alert

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+function onButtonClick() {
+  alert("You clicked the button");
+}
+
+const button = document.createElement("button");
+button.textContent = "Click";
+
+button.addEventListener("click", onButtonClick);
+
+document.body.appendChild(button);
